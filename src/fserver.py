b43d8960b4ad55f49bd27a9de85ffd481b159570
vabhi <cvabhi94@gmail.com>
2021-06-21 09:58:21 -0700

    flask templete

--- src/fserver.py
+++ src/fserver.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
 
 #  using decprator here we are def a function and retun afunction
+#  giving html file to the route and making flask templete
 
 @app.route("/")
 def hello_world():
-    return "Hiiiii"
+    return render_template('index.html')
+    
 
 #  (/) it define the root to search to go the next page /blog/dogs 
 
@@ -16,4 +18,6 @@ def hello_world():
     return "welcomr to my blog"
 
 #  flask templates giving html file to route 
+
+
 
