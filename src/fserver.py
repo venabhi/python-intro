f035e49fdc6e0839cf0cbbe730aa5f1de0dd6e0b
vabhi <cvabhi94@gmail.com>
2021-06-21 12:32:18 -0700

    add url properties

--- src/fserver.py
+++ src/fserver.py
@@ -1,3 +1,4 @@
+from os import name
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,18 +7,18 @@ print(__name__)
 #  using decprator here we are def a function and retun afunction
 #  giving html file to the route and making flask templete
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
-    
+
+@app.route("/<username>/<int:post_id>")
+def hello_world(username=None, post_id=None):
+    return render_template('firstserver.html', name= name, post_id=None)
+
+
 
 #  (/) it define the root to search to go the next page /blog/dogs 
 
 @app.route("/blog")
-def hello_world():
+def hello_blog():
     return "welcomr to my blog"
 
 #  flask templates giving html file to route 
-
-
 
